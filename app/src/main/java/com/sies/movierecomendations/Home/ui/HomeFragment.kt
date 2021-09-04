5c96d83f59207874fc8f7a431af234bf2aeca9b2
Dark-Knight11 <vedantbhamare11@gmail.com>
2021-09-05 03:25:07 +0530

    incomplete recycler adapter implementation

--- app/src/main/java/com/sies/movierecomendations/Home/ui/HomeFragment.kt
+++ app/src/main/java/com/sies/movierecomendations/Home/ui/HomeFragment.kt
@@ -9,49 +9,64 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.sies.movierecomendations.Home.data.HomeViewModel
+import com.sies.movierecomendations.MovieDetails.ui.MovieDetails
 import com.sies.movierecomendations.R
 import com.sies.movierecomendations.Search.ui.SearchActivity
 import com.sies.movierecomendations.databinding.FragmentHomeBinding
 import com.synnapps.carouselview.ImageListener
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment: Fragment() {
 
-    var imageListener: ImageListener? = null
-    lateinit var binding: FragmentHomeBinding
+    private lateinit var imageListener: ImageListener
+    private lateinit var binding: FragmentHomeBinding
 
-    private val viewModel: HomeViewModel by lazy {
-        ViewModelProvider(this).get(HomeViewModel::class.java)
-    }
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
-
         binding.lifecycleOwner = viewLifecycleOwner
         Log.i("HomeFragment", "onCreateView was called")
 
-        viewModel.images.observe(viewLifecycleOwner, {
+        viewModel.images.observe(viewLifecycleOwner, { it?.let {
             setUpBanner(it)
-        })
+        }})
 
         binding.toolbar.setOnMenuItemClickListener { item ->
             if (item.itemId == R.id.search)
                 startActivity(Intent(context, SearchActivity::class.java))
             super.onOptionsItemSelected(item)
         }
-        binding.popMovies.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        viewModel.popularMovies.observe(viewLifecycleOwner, {
-            binding.popMovies.adapter =
-                PopularMoviesAdapter(it)
+
+        val adapter = HomePageAdapter(MovieClickListener {
+            viewModel.passDetailsToAdapter(it)
         })
 
-        binding.trendingMovies.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        viewModel.trendingMovies.observe(viewLifecycleOwner, {
-            binding.trendingMovies.adapter =
-                PopularMoviesAdapter(it)
+        binding.popMovies.adapter = adapter
+        viewModel.popularMovies.observe(viewLifecycleOwner, {
+            adapter.submitList(it.results)
+        })
+
+//        binding.trendingMovies.adapter = adapter
+        viewModel.getTrending().observe(viewLifecycleOwner, {
+            binding.trendingMovies.adapter = PopularMoviesAdapter(it)
+        })
+
+        viewModel.movies.observe(viewLifecycleOwner, {
+            it?.let {
+                val intent = Intent(context, MovieDetails::class.java)
+                intent.putExtra("backdrop-path", it.backdrop_path)
+                intent.putExtra("title", it.title)
+                intent.putExtra("release-date", it.release_date)
+                intent.putExtra("rating", it.vote_average)
+                intent.putExtra("overview", it.overview)
+                intent.putExtra("id", it.id)
+                startActivity(intent)
+            }
         })
 
         return binding.root
@@ -60,9 +75,9 @@ class HomeFragment: Fragment() {
     // Carousel View
     private fun setUpBanner(banner: List<String>) {
         imageListener = ImageListener { position: Int, imageView: ImageView? ->
-            Glide.with(this)
-                .load(banner[position])
-                .into(imageView!!)
+            imageView?.let {
+                Glide.with(this).load(banner[position]).into(imageView)
+            }
         }
         binding.carousel.setImageListener(imageListener)
         binding.carousel.pageCount = banner.size
